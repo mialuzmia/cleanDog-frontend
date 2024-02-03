f948b22c9ae348af66b16a5f0aed01fea53e3bc1
Mia Azevedo <mialuzazevedo@gmail.com>
2024-02-02 20:38:57 -0400

    feat: added states to handle the values in the search form

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,23 +1,51 @@
+import { useState } from 'react';
 import styles from '../styles/components/search.module.scss';
 
 const Search = () => {
+  const [date, setDate] = useState('');
+  const [bigDogNumber, setBigDogNumber] = useState(0);
+  const [smallDogNumber, setSmallDogNumber] = useState(0);
+
+  const handleDateChange = (e) => {
+    setDate(new Date(e.target.value + 'T00:00:00'));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(date);
+    console.log(date.getDay());
+    console.log('small dogs: ', smallDogNumber);
+    console.log('big dogs: ', bigDogNumber);
+
+  }
+
   return (
-    <div className={styles.search__container}>
+    <form className={styles.search__container} onSubmit={handleSubmit}>
       <label className={styles.date__container}>
         <span>Qual dia você quer dar banho no seu doguinho?</span>
-        <input type="date" />
+        <input type="date" onChange={handleDateChange} value={date}/>
       </label>
 
       <label className={styles.number__container}>
         <span>Quantos cachorros pequenos você tem?</span>
-        <input type="number" />
+        <input 
+          type="number" 
+          onChange={e => setSmallDogNumber(e.target.value)} 
+          value={smallDogNumber}
+        />
       </label>
 
       <label className={styles.number__container}>
       <span>Quantos cachorros granders você tem?</span>
-        <input type="number" />
+        <input 
+          type="number" 
+          onChangeCapture={e => setBigDogNumber(e.target.value)} 
+          value={bigDogNumber}
+        />
       </label>
-    </div>
+
+      <button type='submit'>submit</button>
+    </form>
   )
 }
 
