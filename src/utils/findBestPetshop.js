a7eed8a019e34a16ae7adfb11b53a0c8f01bab7d
Mia Azevedo <mialuzazevedo@gmail.com>
2024-02-02 22:34:42 -0400

    feat: created findBestPetshoṕ function

--- src/utils/findBestPetshop.js
+++ src/utils/findBestPetshop.js
@@ -1,72 +1,37 @@
-const petshops = [
-  {
-    "weekdayPrices": {
-      "small": 15,
-      "big": 50
-    },
-    "weekendPrices": {
-      "small": 20,
-      "big": 55
-    },
-    "_id": "65bc8deaf15f9699af20a22d",
-    "name": "Vai Rex",
-    "image": "https://images.pexels.com/photos/16991194/pexels-photo-16991194/free-photo-of-animal-bicho-cachorro-cao.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "distance": 1700,
-    "createdAt": "2024-02-02T06:38:34.659Z",
-    "updatedAt": "2024-02-02T06:38:34.659Z",
-    "__v": 0
-  },
-  {
-    "weekdayPrices": {
-      "small": 30,
-      "big": 45
-    },
-    "weekendPrices": {
-      "small": 30,
-      "big": 45
-    },
-    "_id": "65bd5dd1c953ebf021eaee62",
-    "name": "ChowChawgas",
-    "image": "https://images.pexels.com/photos/7176277/pexels-photo-7176277.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "distance": 800,
-    "createdAt": "2024-02-02T21:25:37.671Z",
-    "updatedAt": "2024-02-02T21:25:37.671Z",
-    "__v": 0
-  },
-  {
-    "weekdayPrices": {
-      "small": 20,
-      "big": 40
-    },
-    "weekendPrices": {
-      "small": 24,
-      "big": 48
-    },
-    "_id": "65bd5edbc953ebf021eaee66",
-    "name": "Meu Canino Feliz",
-    "image": "https://images.pexels.com/photos/13016002/pexels-photo-13016002.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "distance": 2000,
-    "createdAt": "2024-02-02T21:30:03.656Z",
-    "updatedAt": "2024-02-02T21:30:03.656Z",
-    "__v": 0
-  }
-];
 
+const findBestPetshop = (shops, smallDogsNumber, bigDogsNumber, day) => {
+  if(shops.length === 0){
+    return 'The petshop list is empty';
+  };
 
-const findBestPetshop = (shops, smallDogsNumber, BigDogsNumber, day) => {
+  const firstPrices = getPricesBasedOnTheDay(shops[0], day);
+  let bestPrice = calculateTotalPrice(smallDogsNumber, firstPrices.smallPrice, bigDogsNumber, firstPrices.bigPrice);
+  let bestshop = shops[0];
 
   shops.forEach(shop => {
-  
+    const prices = getPricesBasedOnTheDay(shop, day);
+    let currentPrice = calculateTotalPrice(smallDogsNumber, prices.smallPrice, bigDogsNumber, prices.bigPrice);
 
-    let bestPrice = shop
+    console.log(`\nShop: ${shop.name}. price: ${currentPrice}\n`);
+    if(currentPrice < bestPrice ){
+      bestPrice = currentPrice;
+      bestshop = shop;
+    }
+    else if (currentPrice === bestPrice){
+      console.log(`\nThere was a tie beetween ${shop.name} and ${bestshop.name}\n`);
+      if (shop.distance < bestshop.distance){
+        bestPrice = currentPrice;
+        bestshop = shop;
+      }
+    }
+
   });
-
-
+  
+  return bestshop;
 };
 
-const calculateTotalPrice = (shop, weekend) => {
+const calculateTotalPrice = (smallDogsNumber, smallDogsPrice, bigDogsNumber, bigDogsPrice) => {
   
-
   const totalPrice = (smallDogsNumber * smallDogsPrice) + (bigDogsNumber * bigDogsPrice);
 
   return totalPrice;
@@ -89,9 +54,4 @@ const getPricesBasedOnTheDay = (shop, day) => {
   return { smallPrice, bigPrice };
 }
 
-
-// console.log(calculateTotalPrice(2, 20, 1, 50));
-
-const { weekendPrices: {small, big} } = petshops[0];
-
-console.log(big);
+export default findBestPetshop;
